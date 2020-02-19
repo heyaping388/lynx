a5c386e04e9d75b16ecc7accb6dfc13006e4ae81
heyaping <774630093@qq.com>
2020-02-19 15:48:32 +0800

    feat：增加字段脱敏功能

---
+++ lynx-common/lynx-common-core/src/main/java/com/xhe/lynx/common/core/sensitive/SensitiveTypeEnum.java
@@ -0,0 +1,54 @@
+package com.xhe.lynx.common.core.sensitive;
+
+/**
+ * @Auther: xhe
+ * @Date: 2019/12/3 10:50
+ * @Description: 敏感信息枚举类
+ */
+public enum SensitiveTypeEnum {
+
+    /**
+     * 自定义
+     */
+    CUSTOMER,
+    /**
+     * 用户名, 刘*华, 徐*
+     */
+    CHINESE_NAME,
+    /**
+     * 身份证号, 110110********1234
+     */
+    ID_CARD,
+    /**
+     * 座机号, ****1234
+     */
+    FIXED_PHONE,
+    /**
+     * 手机号, 176****1234
+     */
+    MOBILE_PHONE,
+    /**
+     * 地址, 北京********
+     */
+    ADDRESS,
+    /**
+     * 电子邮件, s*****o@xx.com
+     */
+    EMAIL,
+    /**
+     * 银行卡, 622202************1234
+     */
+    BANK_CARD,
+    /**
+     * 密码, 永远是 ******, 与长度无关
+     */
+    PASSWORD,
+    /**
+     * 密钥, 永远是 ******, 与长度无关
+     */
+    KEY,
+    /**
+     * 证件号码  身份证、护照、港澳通行证等。。。
+     */
+    CERT_NO
+}
